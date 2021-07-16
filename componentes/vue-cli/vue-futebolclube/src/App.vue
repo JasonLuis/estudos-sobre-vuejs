70af2fd0ecdd3f03b1462aae2e9c55132ee5eece
JasonLuis <jason20luis@gmail.com>
2021-07-16 15:01:37 -0300

    commit do modulo de components

---
+++ componentes/vue-cli/vue-futebolclube/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <FuteHeader />
+  </div>
+</template>
+
+<script>
+
+import FuteHeader from './components/FuteHeader';
+
+export default {
+  name: 'App',
+  components: {
+    FuteHeader
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Rajdhani&display=swap');
+</style>
---
+++ componentes/vue-cli/vue-futebolclube/src/components/FuteHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+      <div class="container">
+          <nav class="navbar navbar-expand">
+              <a href="" class="link navbar-brand mr-auto">
+                  <img src="./../assets/logo.svg" alt="Hcode Treinamentos" class="img d-inline-block align-top" id="logo" /> Futebol Clube
+              </a>
+
+              <div class="links mr-2">
+                  <a href="" class="btn mr-5">Início</a>
+                  <a href="" class="btn mr-5"></a>
+                  <a href="" class="btn mr-5"></a>
+              </div>
+          </nav>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
